ae69e0bfb9b7b9270958c91819327b97fef4d646
home <andreas.uretschnig@gmail.com>
2026-03-15 16:41:35 +0100

    fix name inconsistencies

--- provider/modules.php
+++ provider/modules.php
@@ -8,10 +8,12 @@ declare(strict_types=1);
 // use EzPhp\Orm\Schema\SchemaServiceProvider;
 // use EzPhp\Cache\CacheServiceProvider;
 // use EzPhp\Events\EventServiceProvider;
+// use EzPhp\Logging\LogServiceProvider;
 
 return [
     // EzPhp\Orm\ModelServiceProvider::class,
     // EzPhp\Orm\Schema\SchemaServiceProvider::class,
     // EzPhp\Cache\CacheServiceProvider::class,
     // EzPhp\Events\EventServiceProvider::class,
+    // EzPhp\Logging\LogServiceProvider::class,
 ];
